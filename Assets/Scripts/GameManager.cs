e1488a68c7519a3cf8510cc7328c79f41889e6e5
wan-eiee <147536673+wan-eiee@users.noreply.github.com>
2023-10-15 02:09:56 +0900

    updated title scene
    title and gamemanager scripts are modified.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    private string playername;
+    public string playerName{
+        get{return playername;}
+        set{
+            if(playername == ""){
+                playername = "default name";
+            }
+            else{
+                playername = value;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class SaveData{
+        public string savedName;
+    }
+
+
+    void Awake(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +50,37 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SaveUserData(){
+        SaveData data = new SaveData
+        {
+            savedName = playerName
+        };
+        string json = JsonUtility.ToJson(data);
+        string path = Application.persistentDataPath + "/savefile.json";
+        File.WriteAllText(path, json);
+    }
+
+    public void LoadUserData(){
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path)){
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            playerName = data.savedName;
+        }
+        else{
+            Debug.Log("No save file.");
+        }
+    }
+
+    public void Exit(){
+        SaveUserData();
+
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit();
+        #endif
     }
 }
--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
 
 public class Title : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject inputFieldObj;
+
+    [SerializeField]
+    private TextMeshProUGUI countDownText;
+    private TMP_InputField inputField;
+    private int timer = 3;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputField = GameObject.Find("InputField").GetComponent<TMP_InputField>();
+        Debug.Log(GameManager.instance);
+        GameManager.instance.LoadUserData();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //call end edit
+    public void GetPlayerName(){
+        GameManager.instance.playerName = inputField.text;
+        InitializeInputField();
+    }
+
+    private void InitializeInputField(){
+        inputField.text = "";
+        inputFieldObj.SetActive(false);
+    }
+
+    public void LoadMainScene(){
+        StartCoroutine(nameof(CountDown));
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator CountDown(){
+        countDownText.enabled = true;
+
+        for(int i = 0; i < timer; i++){
+            if((timer - i) > 1){
+                countDownText.text = "After " + (timer - i).ToString() + " seconds, Load Main Scene.";
+            }
+            else{
+                countDownText.text = "After " + (timer - i).ToString() + " second, Load Main Scene.";
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
